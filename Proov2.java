1f6bfa700fc61c8171d2b25249fd579d8c3a98e9
Jaagup Kippar <jaagup@tlu.ee>
2015-03-13 11:43:05 +0200

    Vooluallikas

---
+++ Proov2.java
@@ -0,0 +1,8 @@
+public class Proov2{
+   public static void main(String[] arg){
+      TakistusKomponent t=new Takisti220oomi();
+      Vooluallikas telefonilaadija=new Laadija(5, 1);
+      telefonilaadija.yhendaTarbija(t);
+      System.out.println(telefonilaadija.kysiVool());
+   }
+}
