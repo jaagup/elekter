2e358c5387fd6e97b7c739f543cfe1ecb28bec45
Jaagup Kippar <jaagup@tlu.ee>
2015-03-13 11:26:41 +0200

    Takisti

--- Takisti220oomi.java
+++ Takisti220oomi.java
@@ -3,3 +3,6 @@ public class Takisti220oomi extends TakistusKomponent{
      return 220;
   }
 }
+
+//Looge klass Takisti, mille konstruktori parameetrina antakse tema takistus oomides
+//Katseta Takistit proovifailis
