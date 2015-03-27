140e7534582251a26fcbb0377c03c831f5daa30e
Jaagup Kippar <jaagup@greeny.cs.tlu.ee>
2015-03-27 10:20:09 +0200

    Jada proov

---
+++ Proov3.java
@@ -0,0 +1,8 @@
+public class Proov3{
+  public static void main(String[] arg){
+     JadaYhendus j=new JadaYhendus();
+     j.lisa(new Takisti220oomi());
+     j.lisa(new Takisti220oomi());
+     System.out.println(j.kysiTakistus());
+  }
+}
